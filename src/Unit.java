8975da8c7718ca769defd28bf92ee1e30a378465
CarlosVogt <carlosvogt15@gmail.com>
2020-05-25 18:33:37 -0300

    teste 2

--- src/Unit.java
+++ src/Unit.java
@@ -13,7 +13,7 @@ public class Unit {
 		System.out.println("|     4. Circular-fit                                                      |");
 		System.out.println("|                                                                          |");
 		System.out.println("----------------------------------------------------------------------------");
-		System.out.println("TESTE");
+		System.out.println("TESTE aaa");
 
 		Scanner scan = new Scanner(System.in);
 		String valueCommand = scan.nextLine();
